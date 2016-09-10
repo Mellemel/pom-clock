dc77d313d9f8c6232f6323ca8d79f14124938a30
Melvin Canela <M.Canela92@outlook.com>
2016-09-10 01:00:43 -0400

    unit tests completed for pomodoro.js

--- src/components/pomodoro.js
+++ src/components/pomodoro.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import Button from './button';
 
 class Pomodoro extends Component {
+  constructor() {
+    super();
+    this.state = { breakTime: 5, sessionTime: 25, timeLeft:25 };
+    this.decreaseTime = this.decreaseTime.bind(this);
+    this.increaseTime = this.increaseTime.bind(this);
+  }
+  decreaseTime(prop) {
+    let propVal = this.state[prop] - 1;
+    this.setState({ [prop]: propVal });
+  }
+  increaseTime(prop) {
+    let propVal = this.state[prop] + 1;
+    this.setState({ [prop]: propVal });
+  }
   render() {
     return (
       <div>
+        <Button icon="menu-left" onClick={() => this.decreaseTime('breakTime') } />
+        <input type="number" value={this.state.breakTime} />
+        <Button icon="menu-right" onClick={() => this.increaseTime('breakTime') } />
+
+        <h3 className="time-left">{this.state.timeLeft}</h3>
         
+        <Button icon="menu-left" onClick={() => this.decreaseTime('sessionTime') } />
+        <input type="number" value={this.state.sessionTime} />
+        <Button icon="menu-right" onClick={() => this.increaseTime('sessionTime') } />
       </div>
     );
   }
--- src/components/pomodoro.spec.js
+++ src/components/pomodoro.spec.js
@@ -1,53 +1,60 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {expect} from 'chai';
 import Pomodoro from './pomodoro';
+import Button from './button';
 
 describe("<Pomodoro />", () => {
   it("should display two inputs", () => {
     const wrapper = shallow(<Pomodoro />);
-    expect(wrapper.find(<input />)).to.have.length(2);
+    expect(wrapper.find('input')).to.have.length(2);
   });
 
   it("should display four buttons", () => {
     const wrapper = shallow(<Pomodoro />);
-    expect(wrapper.find(<button />)).to.have.length(4);
+    expect(wrapper.find('Button')).to.have.length(4);
   });
 
   it("should display the time left", () => {
     const wrapper = shallow(<Pomodoro />);
-    expect(wrapper.find('.timeleft')).to.have.length(1);
+    expect(wrapper.find('.time-left').text()).to.have.length.greaterThan(0);
   });
 
-  it('should decrease time when minus button is pressed for break length and session length', () => {
-    const wrapper = shallow(<Pomodoro />);
-    const buttons = wrapper.findWhere(n => n.type() == 'button' && n.text() == '-');
-    const inputs = wrapper.find(<input />);
+  it('should decrease both break and session length time when minus button is clicked', () => {
+    const wrapper = mount(<Pomodoro />);
+    const buttons = wrapper.findWhere(n => n.type() == Button && n.prop('icon') == 'menu-left');
+    const inputs = wrapper.find('input');
     var before = [];
     var after = [];
     inputs.forEach((input) => {
-      before.push(parseInt(input.text()));
+      before.push(input.prop('value'));
     });
     buttons.forEach((button) => {
       button.simulate('click');
+    });
+    inputs.forEach((input) => {
+      after.push(input.prop('value'));
     });
     expect(before[0]).to.be.greaterThan(after[0]);
     expect(before[1]).to.be.greaterThan(after[1]);
   });
 
-  it('should increase the time when button is pressed for the break length and session length', () => {
-    const wrapper = shallow(<Pomodoro />);
-    const buttons = wrapper.findWhere(n => n.type() == 'button' && n.text() == '+');
-    const inputs = wrapper.find(<input />);
+  it('should increase both break and session length time when plus button is clicked', () => {
+    const wrapper = mount(<Pomodoro />);
+    const buttons = wrapper.findWhere(n => n.type() == Button && n.prop('icon') == 'menu-left');
+    const inputs = wrapper.find('input');
     var before = [];
     var after = [];
     inputs.forEach((input) => {
-      before.push(parseInt(input.text()));
+      before.push(input.prop('value'));
     });
     buttons.forEach((button) => {
       button.simulate('click');
     });
-    expect(before[0]).to.be.lessThan(after[0]);
-    expect(before[1]).to.be.lessThan(after[1]);
+    inputs.forEach((input) => {
+      after.push(input.prop('value'));
+    });
+    expect(before[0]).to.be.greaterThan(after[0]);
+    expect(before[1]).to.be.greaterThan(after[1]);
   });
 });
